b853f975ea3cbab96d849289172c795a23af9f04
zc <zachcaceres@gmail.com>
2020-02-06 14:22:52 -0500

    remove some useless stuff

--- app/Routes.js
+++ app/Routes.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import { Switch, Route } from 'react-router';
-import routes from './constants/routes.json';
-import App from './containers/App';
-import HomePage from './containers/HomePage';
-import CounterPage from './containers/CounterPage';
+import React from 'react'
+import { Switch, Route } from 'react-router'
+import routes from './constants/routes.json'
+import App from './containers/App'
+import HomePage from './containers/HomePage'
 
 export default () => (
   <App>
     <Switch>
-      <Route path={routes.COUNTER} component={CounterPage} />
       <Route path={routes.HOME} component={HomePage} />
     </Switch>
   </App>
--- app/components/Home.js
+++ app/components/Home.js
@@ -1,20 +1,9 @@
-// @flow
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import routes from '../constants/routes.json';
-import styles from './Home.css';
+import React from 'react';
 
-type Props = {};
-
-export default class Home extends Component<Props> {
-  props: Props;
-
-  render() {
-    return (
-      <div className={styles.container} data-tid="container">
-        <h2>Home</h2>
-        <Link to={routes.COUNTER}>to Counter</Link>
-      </div>
-    );
-  }
+export default function Home() {
+  return (
+    <div>
+      <h2>Sup</h2>
+    </div>
+  );
 }
