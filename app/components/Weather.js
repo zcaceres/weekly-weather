9af43a940fcc646ba08bdc13f7de518f0b8ae91f
Jason Patnick <jpatnick10@gmail.com>
2020-02-06 23:03:11 -0500

    feat: integrated weather api

--- app/components/Weather.js
+++ app/components/Weather.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useEffect,useState} from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import FAKE_WEATHER from '../sample/weather.json'
 import WeatherCard from './WeatherCard'
 import FlexboxRow from './FlexboxRow'
 import styled from 'styled-components';
+import axios from 'axios'
 
 const Wrapper = styled.div`
   position: relative;
@@ -30,6 +31,29 @@ const isEven = num => num % 2 === 0;
 export default function Weather({
   location
 }) {
+  const [weatherData, setWeatherData]=useState(null)
+
+  useEffect(()=>{
+    const latitude=location.geometry.location.lat
+    const longitude=location.geometry.location.lng
+
+    axios.get(`https://api.weather.gov/points/${latitude},${longitude}`)
+        .then(function (response) {
+          return axios.get(response.data.properties.forecast);
+        })
+        .then(function(response){
+            setWeatherData(response.data)
+        })
+
+    // address
+    // get long lat
+    // https://api.weather.gov/points/long,lat
+    // get forecast
+    // returns data
+
+    // https://api.weather.gov/points/39.7456,-97.0892
+  })
+
   if (!location) return <Redirect to="/" />;
 
   const {
@@ -37,10 +61,20 @@ export default function Weather({
     address_components
   } = location
 
-  const dayObjects = FAKE_WEATHER.properties.periods
+
+  if(weatherData==null){
+    return(
+      <Wrapper>
+        loading
+      </Wrapper>
+    )
+  }
+  else{
+
+  const dayObjects = weatherData.properties.periods
     .filter(({ number }) => !isEven(number));
 
-  const nightObjects = FAKE_WEATHER.properties.periods
+  const nightObjects = weatherData.properties.periods
     .filter(({ number }) => isEven(number));
 
   const [readableAddress] = address_components
@@ -49,17 +83,18 @@ export default function Weather({
 
   const header = readableAddress || 'Your Weather';
 
-  return (
-    <Wrapper>
-      <NavBar>{header}</NavBar>
-      <FlexboxRow isScrollable>
-        {dayObjects && dayObjects.map((dayWeather, index) => (
-          <WeatherCard
-            dayWeather={dayWeather}
-            nightWeather={nightObjects[index]}
-          />
-        ))}
-      </FlexboxRow>
-    </Wrapper>
-  );
+    return (
+      <Wrapper>
+        <NavBar>{header}</NavBar>
+        <FlexboxRow isScrollable>
+          {dayObjects && dayObjects.map((dayWeather, index) => (
+            <WeatherCard
+              dayWeather={dayWeather}
+              nightWeather={nightObjects[index]}
+            />
+          ))}
+        </FlexboxRow>
+      </Wrapper>
+    );
+  }
 }
